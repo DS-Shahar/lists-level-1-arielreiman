864f43f2010beedc73cb7218598193abec03c529
Ariel Reiman <160618288+arielreiman@users.noreply.github.com>
2024-11-29 18:51:37 +0200

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,122 @@
-class Main {
-  public static void main(String[] args) {
-    System.out.println("Hello World!");
-  }
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+
+        Node<Integer> head = ex1(new int[]{1, 2, 3, 4, 5});
+        
+        System.out.println("ex2");
+        ex2_1(head);
+        
+        ex2_2(head);
+
+        ex2_3(head);
+        
+        System.out.println("ex3");
+        System.out.println(ex3());
+        
+        System.out.println("ex4");
+        ex4(head);
+        
+        System.out.println("ex5");
+        boolean exists = ex5(head, 3);
+        System.out.println("Exists: " + exists);
+        
+        System.out.println("ex6");
+
+        System.out.println(ex6(head, 2));
+
+    }
+
+    public static Node<Integer> ex1(int[] a) {
+        Node<Integer> h = new Node<>(-1);
+        Node<Integer> p = h;
+        Node<Integer> x;
+        for (int i = 0; i < a.length; i++) {
+            x = new Node<>(a[i]);
+            p.setNext(x);
+            p = p.getNext();
+        }
+        return h.getNext();
+    }
+    
+
+    public static void ex2_1(Node<Integer> head) {
+        Node<Integer> current = head;
+        while (current != null) {
+            System.out.println(current.getValue());
+            current = current.getNext();
+        }
+    }
+
+    public static void ex2_2(Node<Integer> current) {
+        if (current != null) {
+            System.out.println(current.getValue());
+            ex2_2(current.getNext());
+        }
+    }
+    
+    public static void ex2_3(Node<Integer> current) {
+        if (current != null) {
+            ex2_2(current.getNext());
+            System.out.println(current.getValue());
+        }
+    }
+    
+    public static Node<Integer> ex3() {
+        Scanner input = new Scanner(System.in);
+
+        Node<Integer> head = null;
+        Node<Integer> p = null;
+        System.out.println(" | Enter numbers to build a list, end with -1 : | ");
+        int msg = input.nextInt();
+        while (msg != -1) { 
+            Node<Integer> newNode = new Node<>(msg);
+            if (head == null) {
+                head = newNode;  
+            } else {
+                p.setNext(newNode);  
+            }
+            p = newNode;
+            msg = input.nextInt();
+        }
+
+        return head; 
+    }
+
+
+    public static void ex4(Node<Integer> current) {
+        if (current != null) {
+            if (current.getValue() % 2 == 0) {
+                System.out.println(current.getValue());
+            }
+            ex4(current.getNext());
+        }
+    }
+
+    public static boolean ex5(Node<Integer> head, int target) {
+        Node<Integer> current = head;
+        while (current != null) {
+            if (current.getValue() == target) {
+                return true;
+            }
+            current = current.getNext();
+        }
+        return false;
+    }
+    public static Node<Integer> ex6(Node<Integer> head, int target) {
+        if (head != null && head.getValue() == target) {
+            return head.getNext();  
+        }
+        Node<Integer> current = head; 
+        while (current != null && current.getNext() != null) {// בדיקת גישה נוחה לאיבר הבא
+            if (current.getNext().getValue() == target) {
+                current.setNext(current.getNext().getNext()); 
+                return head; 
+            }
+            current = current.getNext(); 
+        }
+		return head;
+    }
+   
 }
